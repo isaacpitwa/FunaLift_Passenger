91e9e75eaca5b80a1f0e8c2474841494fb18ea5f
isaacpitwa <isaacpitwa256@gmail.com>
2019-01-10 15:12:37 +0300

    main layout with Map

---
+++ app/src/main/java/com/witsmind/isaac/funalift_passenger/MapActivity.java
@@ -0,0 +1,33 @@
+package com.witsmind.isaac.funalift_passenger;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+       private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        LatLng initial_pointer =new LatLng(0.347596, 32.582520);
+        mMap.addMarker(new MarkerOptions().position(initial_pointer));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(initial_pointer,15));
+
+    }
+}
