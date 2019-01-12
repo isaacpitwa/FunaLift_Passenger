74244ea3b8e933bac8b8c475dc2a0f07044de807
isaacpitwa <isaacpitwa256@gmail.com>
2019-01-12 12:30:28 +0300

    initial commit driver designs

---
+++ app/src/main/java/com/witsmind/isaac/funalift_passenger/DriverAvailablePassengers.java
@@ -0,0 +1,13 @@
+package com.witsmind.isaac.funalift_passenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverAvailablePassengers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_available_passengers);
+    }
+}
---
+++ app/src/main/java/com/witsmind/isaac/funalift_passenger/DriverPayments.java
@@ -0,0 +1,13 @@
+package com.witsmind.isaac.funalift_passenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverPayments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_payments);
+    }
+}
