86ffe43cfeb789a269153d9385aa37bb2fddc088
isaacpitwa <isaacpitwa256@gmail.com>
2019-01-17 18:54:53 +0300

    driver_App XML designs

---
+++ app/src/main/java/com/witsmind/isaac/funalift_passenger/DriverWelcome.java
@@ -0,0 +1,13 @@
+package com.witsmind.isaac.funalift_passenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverWelcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_welcome_activity);
+    }
+}
